e7cc4f2a94d75131c6872bbed5594740f8413922
Akhilesh Varute <akhileshvarute23@gmail.com>
2025-03-01 23:47:19 +0530

    Fixed the package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages
-  server:{
-    host:'0.0.0.0',
+  base: '/',  // For user site (username.github.io)
+  server: {
+    host: '0.0.0.0',
   }
 })
